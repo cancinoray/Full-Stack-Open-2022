26fc9c26c2fc702fddf51d4eb431c4b41d253c8b
Ray Cancino <cancinoray@gmail.com>
2022-11-19 20:50:45 +0800

    add part1 and courseinfo

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Full-Stack-Open-2022
+
---
+++ part1/courseinfo/src/Part.js
@@ -0,0 +1,11 @@
+const Part = (prop) => {
+  return(
+    <div>
+    <p>
+      { prop.part } { prop.exercises }
+    </p>
+    </div>
+  );
+}
+
+export default Part;
